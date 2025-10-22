cd730007d779a5ac51166494eeab8778a64d449f
dharmy05 <dharmy.dev05@gmail.com>
2025-10-22 12:17:56 +0100

    testing all route

--- index.php
+++ index.php
@@ -11,16 +11,16 @@ $method = $_SERVER['REQUEST_METHOD'];
 $uri = rtrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 
 switch ($method) {
-    // case 'GET':
-    //     if ($uri === '/strings/filter-by-natural-language') {
-    //         handleNaturalLanguageFilter($conn);
-    //     } elseif ($uri === '/strings') {
-    //         handleGetAllStrings($conn);
-    //     } elseif (preg_match('#^/strings/(.+)$#', $uri)) {
-    //         $value = rawurldecode(substr($uri, strlen('/strings/')));
-    //         handleGetString($conn, $value);
-    //     }
-    //     break;
+    case 'GET':
+        if ($uri === '/strings/filter-by-natural-language') {
+            handleNaturalLanguageFilter($conn);
+        } elseif ($uri === '/strings') {
+            handleGetAllStrings($conn);
+        } elseif (preg_match('#^/strings/(.+)$#', $uri)) {
+            $value = rawurldecode(substr($uri, strlen('/strings/')));
+            handleGetString($conn, $value);
+        }
+        break;
     case 'POST':
         if ($uri === '/strings') {
             handleCreateString($conn);
@@ -28,12 +28,12 @@ switch ($method) {
         break;
 
 
-    // case 'DELETE':
-    //     if (preg_match('#^/strings/(.+)$#', $uri)) {
-    //         $value = rawurldecode(substr($uri, strlen('/strings/')));
-    //         handleDeleteString($conn, $value);
-    //     }
-    //     break;
+    case 'DELETE':
+        if (preg_match('#^/strings/(.+)$#', $uri)) {
+            $value = rawurldecode(substr($uri, strlen('/strings/')));
+            handleDeleteString($conn, $value);
+        }
+        break;
 
     default:
         http_response_code(404);
